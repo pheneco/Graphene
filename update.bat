018e51062f77db94b190e4a42f8857c29afbb8e5
Trevor Hoglund <trewbot@phene.co>
2015-10-13 20:56:44 -0500

    Added npm install, update.js, etc

--- update.bat
+++ update.bat
@@ -11,15 +11,21 @@ for /f "delims=" %%i in ('node cfg.js %sub%ClientDir') do set clientDir=%%i
 
 echo fetching from github
 git remote add origin https://github.com/Trewbot/Graphene.git >nul 2>&1
-git fetch origin master
+git fetch origin
 
 echo updating client changelog
-%mongoDir%\mongorestore --drop --db %sub%phene %serverDir%\changelog\webClientChanges.bson
+%mongoDir%\mongorestore --drop --db %sub%phene %serverDir%\changelog\webClientChanges.bson >nul 2>&1
 
 echo updating server changelog
-%mongoDir%\mongorestore --drop --db %sub%phene %serverDir%\changelog\serverChanges.bson
+%mongoDir%\mongorestore --drop --db %sub%phene %serverDir%\changelog\serverChanges.bson >nul 2>&1
 
 echo copying client code
 xcopy %serverDir%\client\ %clientDir% /e /h /c /r /q /y >nul
+
+echo running npm install
+npm install
+
+echo running update commands
+node update.js
 
 backup
