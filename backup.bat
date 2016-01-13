28ef61748915b0090882417efdda081a6b8282ac
Trevor Hoglund <trewbot@phene.co>
2016-01-12 20:18:27 -0600

    Modified config and commands to be ran standalone

--- backup.bat
+++ backup.bat
@@ -6,23 +6,23 @@ echo backing up database
 echo reading config.json
 for /f "delims=" %%i in ('node cfg.js backupDir') do set backupDir=%%i
 for /f "delims=" %%i in ('node cfg.js mongoDir') do set mongoDir=%%i
-for /f "delims=" %%i in ('node cfg.js sub') do set sub=%%i
+for /f "delims=" %%i in ('node cfg.js database') do set database=%%i
 for /f "delims=" %%i in ('node cfg.js sevenZipDir') do set sevenZipDir=%%i
-for /f "delims=" %%i in ('node cfg.js %sub%ServerDir') do set serverDir=%%i
+for /f "delims=" %%i in ('node cfg.js serverDir') do set serverDir=%%i
 
 echo moving to backup directory
 if not exist %backupDir% md %backupDir%
 cd %backupDir%
 
 echo creating backup filename
-set filename=%sub%phene %date% %time%
+set filename=%database% %date% %time%
 set filename=%filename:/=-%
 set filename=%filename:.=_%
 set filename=%filename::=-%
 echo filename "%filename%"
 
 echo dumping backup
-%mongoDir%\mongoDump --out "%filename%" --db %sub%phene >nul 2>&1
+%mongoDir%\mongoDump --out "%filename%" --db %database% >nul 2>&1
 
 echo compressing backup
 "%sevenZipDir%\7z.exe" a -tzip "%filename%.zip" "%filename%" > nul
--- unpackLog.bat
+++ unpackLog.bat
@@ -1,28 +1,15 @@
 @echo off
 
 echo.
-echo updating code
+echo unpacking changelog
 
 echo reading config.json
 for /f "delims=" %%i in ('node cfg.js mongoDir') do set mongoDir=%%i
 for /f "delims=" %%i in ('node cfg.js database') do set database=%%i
 for /f "delims=" %%i in ('node cfg.js serverDir') do set serverDir=%%i
 
-echo fetching from github
-git remote add origin https://github.com/Trewbot/Graphene.git >nul 2>&1
-git fetch --all
-git reset --hard origin/master
-
 echo updating client changelog
 %mongoDir%\mongorestore --drop --db %database% %serverDir%\changelog\webClientChanges.bson >nul 2>&1
 
 echo updating server changelog
 %mongoDir%\mongorestore --drop --db %database% %serverDir%\changelog\serverChanges.bson >nul 2>&1
-
-echo running npm install
-call npm install
-
-echo running update commands
-node update.js
-
-backup
