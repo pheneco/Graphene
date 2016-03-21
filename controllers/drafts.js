62249dc962010ca4e2768919d7051b8972c11261
Trevor J Hoglund <trewbot@phene.co>
2016-03-20 19:44:06 -0500

    s0.5.0.0345

---
+++ controllers/drafts.js
@@ -0,0 +1,21 @@
+/*
+ *	Graphene >> Drafts Routes
+ *	Written by Trevor J Hoglund
+ *	Mar 20, 2016
+ */
+ 
+module.exports = function(app, Graphene){
+	var User	= require('../models/user'),
+		Draft	= require('../models/draft');
+	
+	//	Routes
+	app.get('/drafts',function(req,res){
+		
+	});
+	app.get('/draft',function(req,res){
+		
+	});
+	app.post('/drafts/new',function(req,res){
+		
+	});
+}
