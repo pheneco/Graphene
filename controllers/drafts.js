b966f90e59d52e7aae847f2b083442c59a319fa1
Trevor J Hoglund <trewbot@phene.co>
2016-03-21 14:13:46 -0500

    s0.5.0.0346

--- controllers/drafts.js
+++ controllers/drafts.js
@@ -1,7 +1,7 @@
 /*
  *	Graphene >> Drafts Routes
  *	Written by Trevor J Hoglund
- *	Mar 20, 2016
+ *	Mar 21, 2016
  */
  
 module.exports = function(app, Graphene){
@@ -10,12 +10,26 @@ module.exports = function(app, Graphene){
 	
 	//	Routes
 	app.get('/drafts',function(req,res){
-		
+		Draft.find({user:req.session.user},null,{sort:{date:-1}},function(e,d){
+			var drafts = [];
+			for(var i = 0; i < d.length; d++)
+				drafts[i]	= {
+					name	: d[i].name,
+					text	: d[i].plainText,
+					date	: d[i].lastEdit
+				};
+			res.send(drafts);
+		});
 	});
-	app.get('/draft',function(req,res){
-		
+	app.get('/draft/:id',function(req,res){
+		//	Get a single draft based on id
 	});
 	app.post('/drafts/new',function(req,res){
-		
+		var draft	= new Draft({
+			user		: req.session.user,
+			name		: req.body.name,
+			plainText	: req.body.text,
+			lastEdit	: +(new Date())
+		});
 	});
 }
